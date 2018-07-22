57c843e1bf6d4a5b6a1b65723cfab1b1f06f3730
hottar <hottar@rams.colostate.edu>
2018-07-22 16:05:02 -0600

    Create PlayableCharacter.cpp

---
+++ PlayableCharacter.cpp
@@ -0,0 +1,13 @@
+#include "./PlayableCharacter.h"
+
+public APlayableCharacter::APlayableCharacter(int s) : Speed(s) {
+
+}
+
+public APlayableCharacter::tick(float DeltaSeconds) {
+
+}
+
+private APlayableCharacter::BeginPlay() {
+
+}
