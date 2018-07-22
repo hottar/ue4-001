06ea6a82317ed21b86c43d28b43dbd70274ecb10
hottar <hottar@rams.colostate.edu>
2018-07-22 16:13:59 -0600

    Update PlayableCharacter.h

--- PlayableCharacter.h
+++ PlayableCharacter.h
@@ -9,6 +9,15 @@ public:
   APlayableCharacter();
   virtual void Tick(float DeltaSeconds) override;
   
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Velocity")
+  float Speed;
+  
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Velocity")
+    int Direction;
+  
+  // Transient: data niether saved nor loaded to disk. may be applied to variables dependent of the other.
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category="Velocity")
+    float Acceleration;
 private:
   virtual void BeginPlay() override;
 }
